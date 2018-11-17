b91384e29a59b3a5cb25b3fc805c5da5a4511cab
lululululu <luquanhao@yeah.net>
2018-11-17 17:18:57 +0800

    code synchornize 11/17 sec

--- vjudge/4 J hide handkerchief .cpp
+++ vjudge/4 J hide handkerchief .cpp
@@ -1,3 +1,10 @@
+/*
+    题目贼鸡儿难受
+    其实就是n个人围成的圈从一个人开始每隔m-1个人查看一次
+    能不能查看完
+    也就是求最大公因数
+*/
+//辗转相除法  for求公因数会超时
 #include <iostream>
 using namespace std;
 int main()
@@ -8,15 +15,15 @@ int main()
         int infor;
         while (1)
         {
-            infor = n % m;
-            n = m;
-            if (!infor)
+            infor = n % m; //取余
+            n = m; //记录除数
+            if (!infor)//当余数为0时 跳出
             {
                 break;
             }
-            m = infor;
+            m = infor;//记录余数
         }
-        if (m == 1)
+        if (m == 1)//当最大公因数为1时输出yes
         {
             cout << "YES" << endl;
         }
