e57befc779f66b9587e4c9f642ef6ae87a72231d
lululululu <luquanhao@yeah.net>
2018-11-18 18:00:39 +0800

    code synchornize 11/18 thi

--- vjudge/训练赛11-18/E Two Bags of Potatoes.cpp
+++ vjudge/训练赛11-18/E Two Bags of Potatoes.cpp
@@ -4,13 +4,14 @@ int main()
 {
     int n, k, y, x = 0, flag = 0;
     cin >> y >> k >> n;
-    int yu = y % k;
-    yu=k-yu;
+    int yu = y % k;//取余
+    yu=k-yu;//取差
+    //当差为0时 说明x最小为0
     if (yu == 0)
     {
         cout << "0 ";
         flag = 1;
-        x += k;
+        x += k;//要使可整除 直接加k
         while (x + y <= n)
         {
             cout << x << ' ';
