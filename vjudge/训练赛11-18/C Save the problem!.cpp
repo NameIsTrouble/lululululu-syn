aa263136f89c0f533949fb54f1f97a12ea2d9932
lululululu <luquanhao@yeah.net>
2018-11-18 18:31:06 +0800

    code synchornize 11/18 forth

--- vjudge/训练赛11-18/C Save the problem!.cpp
+++ vjudge/训练赛11-18/C Save the problem!.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 int main()
 {
-    int a;
+    int a,n=1;
     cin>>a;
-    
+    while((n+1)/2!=a)
+    {
+        n++;
+    }
+        cout<<n<<' '<<2<<endl<<1<<' '<<2<<endl;
+    system("pause");
+    return 0;
 }
 //眼瞎 不解释
