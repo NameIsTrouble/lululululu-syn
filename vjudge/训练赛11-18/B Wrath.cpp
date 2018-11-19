188f6a5a5acecc74522e3546ab49c042e3902e3f
lululululu <luquanhao@yeah.net>
2018-11-19 12:16:29 +0800

    code synchornize 11-19 fir

--- vjudge/训练赛11-18/B Wrath.cpp
+++ vjudge/训练赛11-18/B Wrath.cpp
@@ -5,30 +5,27 @@ int main()
     int n;
     cin >> n;
     int *human = new int[n];
-    int *flag = new int[n];
-    int sum = n;
-    for (int i = 1; i <= n; i++)
+    int sum = 1, judge;//最后一个人一定存活
+
+    for (int i = 0; i < n; i++)
     {
         cin >> human[i];
-        flag[i] = 0;
     }
-    for (int i = n; i >= 1; i--)
+
+    judge = human[n - 1];//长度
+
+    for (int i = n - 2; i >= 0; i--)
     {
-        for (int j = max(i - human[i], 1); j < i; j++)
+        if (!judge)//如果长度为0那么下一个人存活
         {
-            if (flag[j] == 0)
-            {
-                sum--;
-                flag[j] = 1;
-            }
+            sum++;
         }
-        if (sum == 1)
-        {
-            break;
-        }
+
+        judge = max(judge - 1, human[i]);//判断下一个人的刀长度与自己刀除去下一个人的长度哪个长 取其长
     }
+
     printf("%d\n", sum);
     system("pause");
     return 0;
 }
-//超时
+//是时候学学时间复杂度了
