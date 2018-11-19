e2682a382fe97b075f57db0092ada185c4ce300b
lululululu <luquanhao@yeah.net>
2018-11-19 22:33:12 +0800

    code synchornize 11-19 sec

--- vjudge/训练赛11-18/E Two Bags of Potatoes.cpp
+++ vjudge/训练赛11-18/E Two Bags of Potatoes.cpp
@@ -1,3 +1,6 @@
+/*
+    水题
+*/
 #include <iostream>
 using namespace std;
 int main()
@@ -5,18 +8,21 @@ int main()
     int n, k, y, x = 0, flag = 0;
     cin >> y >> k >> n;
     int yu = y % k;//取余
-    yu=k-yu;//取差
-    //当差为0时 说明x最小为0
+    yu = k - yu; //取差
+
+    //当差为0时 说明x最小为0 题意x>=1
     if (yu == 0)
     {
-        cout << "0 ";
+        //cout << "0 ";
         flag = 1;
         x += k;//要使可整除 直接加k
+
         while (x + y <= n)
         {
             cout << x << ' ';
             x += k;
         }
+
         cout << endl;
     }
     else
@@ -24,12 +30,14 @@ int main()
         if (y + yu <= n)
         {
             cout << yu << ' ';
-            x = yu+k;
+            x = yu + k;
+
             while (x + y <= n)
             {
                 cout << x << ' ';
                 x += k;
             }
+
             flag = 1;
             cout << endl;
         }
@@ -40,6 +48,7 @@ int main()
             return 0;
         }
     }
+
     system("pause");
     return 0;
 }
--- vjudge/训练赛11-18/F Fox and Box Accumulation.cpp
+++ vjudge/训练赛11-18/F Fox and Box Accumulation.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main()
 {
-    int n,test[110];
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        cin>>test[i];
-    }
-    sort(test,test+n);
+    int n, test[110];
+    cin >> n;
 
+    for(int i = 0; i < n; i++)
+    {
+        cin >> test[i];
+    }
+
+    sort(test, test + n);
 }
-//贪心
