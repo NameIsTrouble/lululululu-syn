dc82af91476626629282abb11626c7653db95a12
NameIsTrouble <luquanhao@yeah.net>
2018-12-15 15:58:43 +0800

    code synchronize 12-15 fir

---
+++ C++/vjudge/LightOJ/1138 Trailing Zeroes (III).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long q;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>q;
+        q=pow(10,q);
+        
+    }
+}
