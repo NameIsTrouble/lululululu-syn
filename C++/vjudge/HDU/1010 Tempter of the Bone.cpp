a847fc6bc2079dd7347b7aabe774e5b4ec35e042
NameIsTrouble <luquanhao@yeah.net>
2019-01-05 21:52:20 +0800

    code synchronize 01-05-19

--- C++/vjudge/HDU/1010 Tempter of the Bone.cpp
+++ C++/vjudge/HDU/1010 Tempter of the Bone.cpp
@@ -1,109 +1,112 @@
-#include <bits/stdc++.h>
+/*
+    深度优先搜索+奇偶剪枝
+*/ 
 #include <iostream>
-#include <queue>
+#include <cmath>
 #include <string.h>
-#include <algorithm>
 using namespace std;
+int sx, sy, ex, ey, n, m, c, flag, cou;
+int mar[8][8], judge[8][8];
+void DFS(int x, int y, int cou)
+{
+    if (mar[x][y] == 3 && cou == c)//如果到达终点满足时间条件
+    {
+        flag = 1;//判断为有
+        return;//返回
+    }
+    if (judge[x - 1][y] && mar[x - 1][y] && x - 1 >= 0)//左移
+    {
+        judge[x - 1][y] = 0;//将该点判为已使用
+        DFS(x - 1, y, cou + 1);//深度搜索
+        judge[x - 1][y] = 1;//还原
+    }
+    if (judge[x + 1][y] && mar[x + 1][y] && x + 1 < n)
+    {
+        judge[x + 1][y] = 0;
+        DFS(x + 1, y, cou + 1);
+        judge[x + 1][y] = 1;
+    }
+    if (judge[x][y - 1] && mar[x][y - 1] && y - 1 >= 0)
+    {
+        judge[x][y - 1] = 0;
+        DFS(x, y - 1, cou + 1);
+        judge[x][y - 1] = 1;
+    }
+    if (judge[x][y + 1] && mar[x][y + 1] && y + 1 < m)
+    {
+        judge[x][y + 1] = 0;
+        DFS(x, y + 1, cou + 1);
+        judge[x][y + 1] = 1;
+    }
+    if (flag)//如果找到 
+    {
+        return;
+    }
+}
 int main()
 {
-    int n, m, c;
-    while (cin >> n >> m >> c && (n && m && c))
+    while (scanf(" %d %d %d", &n, &m, &c) && n && m && c)
     {
-        int Mart[8][8];
-        bool judge[8][8];
-        int step[8][8];
-        queue<int> line;
-        memset(Mart, 0, sizeof(Mart));
-        memset(judge, true, sizeof(judge));
-        memset(step, 0, sizeof(step));
-        int sx, sy, min = 55, max = 0;
+        memset(mar, 0, sizeof(mar));
+        memset(judge, 0, sizeof(judge));
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
                 char x;
-                cin >> x;
+                scanf(" %c", &x);
                 if (x == 'S')
                 {
-                    Mart[i][j] = 2;
-                    judge[i][j] = false;
-                    step[i][j] = 0;
+                    mar[i][j] = 2;
                     sx = i;
                     sy = j;
                 }
                 else if (x == '.')
                 {
-                    Mart[i][j] = 1;
-                }
-                else if (x == 'X')
-                {
-                    Mart[i][j] = 0;
-                    judge[i][j] = false;
+                    judge[i][j] = 1;
+                    mar[i][j] = 1;
                 }
                 else if (x == 'D')
                 {
-                    Mart[i][j] = 3;
+                    judge[i][j] = 1;
+                    mar[i][j] = 3;
+                    ex = i;
+                    ey = j;
                 }
             }
         }
-        line.push(sx);
-        line.push(sy);
-        while (!line.empty())
+        if (abs(sx - ex) + abs(sy - ey) - c > 0)//如果要求时间小于最小时间
         {
-            int x = line.front();
-            line.pop();
-            int y = line.front();
-            line.pop();
-            if (Mart[x][y] == 3)
-            {
-                if (step[x][y] > max)
-                {
-                    max = step[x][y];
-                }
-                if (step[x][y] < min)
-                {
-                    min = step[x][y];
-                }
-                continue;
-            }
-            if (judge[x - 1][y] && x - 1 >= 0)
-            {
-                step[x - 1][y] = step[x][y] + 1;
-                judge[x - 1][y] = false;
-                line.push(x - 1);
-                line.push(y);
-            }
-            if (judge[x + 1][y] && x + 1 < n)
-            {
-                step[x + 1][y] = step[x][y] + 1;
-                judge[x + 1][y] = false;
-                line.push(x + 1);
-                line.push(y);
-            }
-            if (judge[x][y - 1] && y - 1 >= 0)
-            {
-                step[x][y - 1] = step[x][y] + 1;
-                judge[x][y - 1] = false;
-                line.push(x);
-                line.push(y - 1);
-            }
-            if (judge[x][y + 1] && y + 1 < m)
-            {
-                step[x][y + 1] = step[x][y] + 1;
-                judge[x][y + 1] = false;
-                line.push(x);
-                line.push(y + 1);
-            }
+            cout << "NO\n";
+            continue;
         }
-        if (c >= min && c <= max)
+        if ((c - abs(sx - ex) - abs(sy - ey)) % 2 == 1)//当要求时间与最小时间的差值为奇数时
         {
-            cout << "YES\n";
+            cout << "NO\n";
+            continue;
         }
         else
         {
-            cout << "NO\n";
+            flag = 0;
+            DFS(sx, sy, 0);
+            if (flag)
+            {
+                cout << "YES\n";
+            }
+            else
+            {
+                cout << "NO\n";
+            }
         }
     }
     system("pause");
     return 0;
 }
+/*
+    奇偶剪枝
+    可以得知如果要求时间大于最小时间
+    一定为多移了几个位置
+    为了满足坐标要求
+    多的纵坐标和横坐标要再次移动回来
+    其时间一定为偶数
+*/
