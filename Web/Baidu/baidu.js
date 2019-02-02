2c86228e408b9e50a7320be9f9c3ce125f394c7c
NameIsTrouble <luquanhao@yeah.net>
2019-02-02 17:43:57 +0800

    code sybchronize 02-02-19 fir

---
+++ Web/Baidu/baidu.js
@@ -0,0 +1,17 @@
+
+
+function show(li)
+{
+    var temp = li.getElementsByTagName("ul");
+    
+    temp[0].style.display = "block";
+
+}
+
+function hide(li)
+{
+    var temp = li.getElementsByTagName("ul");
+    
+    temp[0].style.display = "none";
+     
+}
