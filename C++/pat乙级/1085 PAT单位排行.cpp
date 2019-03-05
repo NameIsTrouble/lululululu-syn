3842c0481195f2def965767a52cc84419e535022
lululululu <luquanhao@yeah.net>
2019-03-05 20:51:02 +0800

    code synchronize 2019/3/5 sec

--- C++/pat乙级/1085 PAT单位排行.cpp
+++ C++/pat乙级/1085 PAT单位排行.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
-#include <map>
+#include <unordered_map> //使用map会导致最后一个测试点超时
 #include <algorithm>
 #include <vector>
 
 using namespace std;
 
+/*学校的信息*/
 struct node
 {
     int BScore, AScore, TScore, TotalScore, Num, Rank;
     string school;
 };
 
-map<string, node> list;
-vector<node> ans;
+unordered_map<string, node> list; //学校
+vector<node> ans;                 //整理后的学校
 
-string ToLowerCase(string a)
-{
-    string lower;
-
-    for (int i = 0; i < a.size(); i++)
-    {
-        if (isupper(a[i]))
-        {
-            lower[i] = tolower(a[i]);
-        }
-        else
-        {
-            lower[i] = a[i];
-        }
-    }
-
-    lower[a.size()] = '\0';
-    return lower;
-}
-
+/*排序*/
 bool cmp(node a, node b)
 {
     if (a.TotalScore == b.TotalScore)
@@ -57,14 +39,20 @@ int main()
 {
     int n, ns = 0;
     cin >> n;
+
     while (n--)
     {
         string id, col;
         int sco;
-        cin >> id >> sco >> col;
-        string sco = ToLowerCase(col);
-        cout << sco << endl;
+        cin >> id >> sco >> col; //输入准考证号 成绩 学校
 
+        /*将学校名称小写*/
+        for (int i = 0; i < col.size(); i++)
+        {
+            col[i] = tolower(col[i]);
+        }
+
+        /*如果此前没有出现过该学校 那么在map里新建*/
         if (list.find(col) == list.end())
         {
             ns++;
@@ -76,6 +64,7 @@ int main()
             list[col].school = col;
         }
 
+        /*按照考试类型加分*/
         switch (id[0])
         {
         case 'A':
@@ -89,39 +78,43 @@ int main()
             break;
         }
 
-        list[col].Num++;
+        list[col].Num++; //学校人数增加
     }
 
-    for (map<string, node>::iterator i = list.begin(); i != list.end(); i++)
+    /*遍历map将学校总成绩算出 并插入到vector里*/
+    for (unordered_map<string, node>::iterator i = list.begin(); i != list.end(); i++)
     {
         i->second.TotalScore = i->second.AScore + i->second.BScore / 1.5 + i->second.TScore * 1.5;
         ans.push_back(i->second);
-        cout << i->second.TotalScore << endl;
     }
 
-    sort(ans.begin(), ans.end(), cmp);
-    ans[0].Rank = 1;
-    cout << ns << endl;
+    sort(ans.begin(), ans.end(), cmp); //排序
+    ans[0].Rank = 1;                   //设置第一个学校排名为1
+    cout << ns << endl;                //输出数组里的学校总数
 
+    /*遍历输出学校信息*/
     for (int i = 0; i < ns; i++)
     {
+        /*如果是第一个学校 则直接输出信息*/
         if (!i)
         {
-            cout << ans[i].Rank << " " << ans[i].school << " " << ans[i].TotalScore << " " << ans[i].Num << endl; 
+            cout << ans[i].Rank << " " << ans[i].school << " " << ans[i].TotalScore << " " << ans[i].Num << endl;
         }
+        /*如果不是第一个学校*/
         else
         {
+            /*比较总成绩大小 如果相等 则排名与前一个学校相同*/
             if (ans[i].TotalScore == ans[i - 1].TotalScore)
             {
                 ans[i].Rank = ans[i - 1].Rank;
                 cout << ans[i].Rank << " " << ans[i].school << " " << ans[i].TotalScore << " " << ans[i].Num << endl;
             }
+            /*否则计算排名*/
             else
             {
                 ans[i].Rank = i + 1;
                 cout << ans[i].Rank << " " << ans[i].school << " " << ans[i].TotalScore << " " << ans[i].Num << endl;
             }
-            
         }
     }
 
