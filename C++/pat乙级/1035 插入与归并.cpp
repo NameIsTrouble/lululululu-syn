a545612cb0d43273fb584b015e17d670355de962
lululululu <luquanhao@yeah.net>
2019-03-19 21:45:31 +0800

    code synchronize 2019/3/19 sec

---
+++ C++/pat乙级/1035 插入与归并.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string temp, a, b;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; ++i)
+        cin >> a[i];
+
+    a[N] = '\0';
+    b = a;
+
+    
+}
--- C++/pat乙级/1044 火星数字.cpp
+++ C++/pat乙级/1044 火星数字.cpp
@@ -1,44 +1,52 @@
-//WA
 #include <iostream>
 #include <math.h>
 #include <cstring>
+#include <map>
+
 using namespace std;
+
+map<char, string> fir, sec;
+
+void giveval()
+{
+    fir['1'] = "jan";
+    fir['2'] = "feb";
+    fir['3'] = "mar";
+    fir['4'] = "apr";
+    fir['5'] = "may";
+    fir['6'] = "jun";
+    fir['7'] = "jly";
+    fir['8'] = "aug";
+    fir['9'] = "sep";
+    fir['9' + 1] = "otc";
+    fir['9' + 2] = "nov";
+    fir['9' + 3] = "dec";
+    sec['1'] = "tam";
+    sec['2'] = "hel";
+    sec['3'] = "maa";
+    sec['4'] = "huh";
+    sec['5'] = "tou";
+    sec['6'] = "kes";
+    sec['7'] = "hei";
+    sec['8'] = "elo";
+    sec['9'] = "syy";
+    sec['9' + 1] = "lok";
+    sec['9' + 2] = "mer";
+    sec['9' + 3] = "jou";
+}
+
 int main()
 {
-    int n, m, z, v;
-    string fire1[13] = {" ", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
-    string fire2[13] = {" ", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    giveval();
+
+    int N;
+    cin >> N;
+
+    while (N--)
     {
-        int a, num[3], sum = 0, count = 0, cc = 0;
-        char test[100];
-        char c[7];
-        bool flag;
-        while ((a = getchar()) != '\n')
-        {
-            test[count] = a;
-        }
-        if (test[0] >= '0' && test[0] <= '9')
-        {
-            for (int i = count; i >= 0; i--)
-            {
-                sum += num[count] * pow(10, count - i);
-            }
-            while (sum != 0)
-            {
-                m = sum / 13;
-                if (m != 0)
-                {
-                    printf("%s %s\n", fire2[m], fire1[sum % 13]);
-                }
-                else
-                {
-                    printf("%s\n", fire1[sum]);
-                }
-            }
-        }
+        
     }
+
     system("pause");
     return 0;
 }
