7cd089dc0351ebda9033690b8ccc43188234a814
NameIsTrouble <luquanhao@yeah.net>
2019-03-29 13:06:01 +0800

    code update 2019/3/29 fir

--- C++/pat乙级/1006 换个格式输出整数.cpp
+++ C++/pat乙级/1006 换个格式输出整数.cpp
@@ -1,57 +1,31 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
+
 int main()
 {
-	string test;
-	while(cin>>test)
-	{
-		int l=test.size();
-		if(l==3)
-		{
-			for(int i=0;i<test[0]-'0';i++)
-			{
-				cout<<'B';
-			}
-			for(int i=0;i<test[1]-'0';i++)
-			{
-				cout<<'S';
-			}
-			for(int i=1;i<=test[2]-'0';i++)
-			{
-				cout<<i;
-				if(i==test[2]-'0')
-				{
-					cout<<endl;
-				}			
-			}
-		}
-		else if(l==2)
-		{
-			for(int i=0;i<test[0]-'0';i++)
-			{
-				cout<<'S';
-			}
-			for(int i=1;i<=test[1]-'0';i++)
-			{
-				cout<<i;
-				if(i==test[1]-'0')
-				{
-					cout<<endl;
-				}
-			}
-		}
-		else if(l==1)
-		{
-			for(int i=1;i<=test[0]-'0';i++)
-			{
-				cout<<i;
-				if(i==test[0]-'0')
-				{
-					cout<<endl;
-				}
-			}
-		}
-	}
+	int x;
+	cin >> x;
+
+	string num = to_string(x);
+
+	reverse(num.begin(), num.end());
+
+	num.append(3 - num.size(), '0');
+
+	reverse(num.begin(), num.end());
+
+	for (int i = 0; i < num[0] - '0'; ++i)
+		cout << 'B';
+	
+	for (int i = 0; i < num[1] - '0'; ++i)
+		cout << 'S';
+	
+	for (int i = 1; i <= num[2] - '0'; ++i)
+		cout << i;
+	
+	cout << endl;
+
 	return 0;
 }
--- C++/pat乙级/1007 素数对猜想.cpp
+++ C++/pat乙级/1007 素数对猜想.cpp
@@ -1,51 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string.h>
+
 using namespace std;
-bool test(int x)
+
+vector<int> primer;
+bool isprimer[100005];
+
+void GetPrimer(int n)
 {
-	int flag=0;
-	if(x==2||x==3)
-	{
-		return true;
-	}
-	else
-	{
-		for(int j=2;j<=x/2;j++)
+	for (int i = 2; i <= n; ++i)
+		if (isprimer[i])
 		{
-			if(x%j==0)
-			{
-				return false;
-				flag=1;
-				break;
-			}
-			if(j==x/2)
-			{
-				if(flag==0)
-				{
-					return true;
-				}
-			}
+			for (int j = i + i; j < n; j += i)
+				isprimer[j] = false;
+
+			primer.push_back(i);
 		}
-	}
 }
+
 int main()
 {
-	int n,p[100000],c=0,count=0;
-	cin>>n;
-	for(int i=2;i<=n;i++)
-	{
-		if(test(i)==true)
-		{
-			p[c]=i;
-			if(c>=1)
-			{
-				if(p[c]-p[c-1]==2)
-				{
-					count++;
-				}
-			}
-			c+=1;
-		}
-	}
-	cout<<count<<endl;
+	int n;
+	cin >> n;
+
+	memset(isprimer, true, sizeof(isprimer));
+
+	GetPrimer(n);
+	int cnt = 0;
+
+	for (int i = 1; i < primer.size(); ++i)
+		if (primer[i] - primer[i - 1] == 2)
+			cnt++;
+
+	cout << cnt << endl;
+
 	return 0;
 }
