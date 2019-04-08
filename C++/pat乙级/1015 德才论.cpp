a9d1084d3526a0229c5ea68d2eafbec24716fe52
NameIsTrouble <luquanhao@yeah.net>
2019-04-08 16:27:02 +0800

    code update 2019/4/8 fir

--- C++/pat乙级/1015 德才论.cpp
+++ C++/pat乙级/1015 德才论.cpp
@@ -1,67 +1,64 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
+
 struct student
 {
-    int idnum, ds, cs, lev = 0;
+    int idnum, ds, cs, lev;
 } stu[100010];
+
 bool cmp(student a, student b)
 {
     if (a.ds + a.cs == b.ds + b.cs)
-    {
         if (a.ds == b.ds)
-        {
             return a.idnum < b.idnum;
-        }
         else
-        {
             return a.ds > b.ds;
-        }
-    }
     else
-    {
         return (a.ds + a.cs) > (b.cs + b.ds);
-    }
 }
+
 int main()
 {
-    int n, l, h, lev[100010] = {0}, count = 0;
+    int n, l, h, lev[100010] = {0}, cnt = 0;
     scanf("%d%d%d", &n, &l, &h);
+
     for (int i = 0; i < n; i++)
     {
         scanf("%d%d%d", &stu[i].idnum, &stu[i].ds, &stu[i].cs);
         if (stu[i].cs >= h && stu[i].ds >= h)
         {
             stu[i].lev = 1;
-            count++;
+            cnt++;
         }
         else if (stu[i].ds >= h && stu[i].cs < h && stu[i].cs >= l)
         {
             stu[i].lev = 2;
-            count++;
+            cnt++;
         }
         else if (stu[i].cs < h && stu[i].ds < h && stu[i].ds >= stu[i].cs && stu[i].cs >= l && stu[i].ds >= l)
         {
             stu[i].lev = 3;
-            count++;
+            cnt++;
         }
         else if (stu[i].ds >= l && stu[i].cs >= l)
         {
-            count++;
+            cnt++;
             stu[i].lev = 4;
         }
     }
+
     sort(stu, stu + n, cmp);
-    printf("%d\n", count);
+
+    printf("%d\n", cnt);
+
     for (int i = 1; i < 5; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
             if (stu[j].lev == i)
                 printf("%d %d %d\n", stu[j].idnum, stu[j].ds, stu[j].cs);
-        }
-    }
+
     system("pause");
     return 0;
 }
--- C++/pat乙级/1016 部分A+B.cpp
+++ C++/pat乙级/1016 部分A+B.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include <cstring>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
 	string a, b;
-	long long int Da, Db, Pa = 0, Pb = 0, ca = 1, cb = 1;
+	int Da, Db, Pa = 0, Pb = 0;
 	cin >> a >> Da >> b >> Db;
 
-	for (int i = 0; i < a.size(); i++)
+	for (int i = 0; i < a.size(); ++i)
 		if (a[i] - '0' == Da)
-		{
-			Pa += Da * pow(10, ca - 1);
-			ca++;
-		}
+			Pa = Pa * 10 + Da;
 
-	for (int i = 0; i < b.size(); i++)
+	for (int i = 0; i < b.size(); ++i)
 		if (b[i] - '0' == Db)
-		{
-			Pb += Db * pow(10, cb - 1);
-			cb++;
-		}
+			Pb = Pb * 10 + Db;
 
 	cout << Pa + Pb << endl;
+
 	return 0;
 }
