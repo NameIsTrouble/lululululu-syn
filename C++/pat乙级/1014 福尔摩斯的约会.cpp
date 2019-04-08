464a89a23860f57c33d9de3faad645537445fe84
NameIsTrouble <luquanhao@yeah.net>
2019-04-08 17:21:22 +0800

    code update 2019/4/8 sec

--- C++/pat乙级/1014 福尔摩斯的约会.cpp
+++ C++/pat乙级/1014 福尔摩斯的约会.cpp
@@ -1,55 +1,42 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
+string D[7] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
 int main()
 {
-	string DAY[7] = {"MON ", "TUE ", "WED ", "THU ", "FRI ", "SAT ", "SUN "};
 	string s1, s2, s3, s4;
 	cin >> s1 >> s2 >> s3 >> s4;
-	int si1, si2, si3, si4;
-	si1 = s1.length();
-	si2 = s2.length();
-	si3 = s3.length();
-	si4 = s4.length();
-	int i = 0;
-	string en;
+	bool flag = false;
 
-	for (; i < si1 && i < si2; ++i)
-		if (s1[i] == s2[i] && s1[i] >= 'A' && s1[i] <= 'G')
+	for (int i = 0; i < (s1.size() < s2.size() ? s1.size() : s2.size()); ++i)
+		if (s1[i] == s2[i] && s1[i] >= 'A' && s1[i] <= 'G' && !flag)
 		{
-			cout << DAY[s1[i] - 'A'];
-			break;
+			cout << D[s1[i] - 'A'] << " ";
+			flag = true;
 		}
-
-	++i;
-	int hour;
-
-	for (; i < si1 && i < si2; ++i)
-		if (s1[i] == s2[i])
-			if (s1[i] >= '0' && s1[i] <= '9')
+		else if (s1[i] == s2[i] && flag)
+		{
+			if (isdigit(s1[i]))
 			{
-				hour = s1[i] - '0';
-				cout << hour / 10 << hour % 10 << ":";
+				cout << 0 << s1[i] << ":";
 				break;
 			}
 			else if (s1[i] >= 'A' && s1[i] <= 'N')
 			{
-				hour = (s1[i] - 'A' + 10);
-				cout << hour / 10 << hour % 10 << ":";
+				cout << (s1[i] - 'A' + 10) << ":";
 				break;
 			}
+		}
 
-	for (int i = 0; i < si3 && i < si4; ++i)
-		if (s3[i] == s4[i])
-			if ((s3[i] >= 'a' && s3[i] <= 'z') || (s3[i] >= 'A' && s3[i] <= 'Z'))
-			{
-				cout << i / 10 << i % 10 << endl;
-				break;
-			}
+	for (int i = 0; i < (s3.size() < s4.size() ? s3.size() : s4.size()); ++i)
+		if (s3[i] == s4[i] && isalpha(s3[i]))
+		{
+			cout << setw(2) << setfill('0') << i << endl;
+			break;
+		}
 
-	system("pause");
 	return 0;
 }
