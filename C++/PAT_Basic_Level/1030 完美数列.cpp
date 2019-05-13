259a10666e697fa66961dcf7db133dfcacde7650
NameIsTrouble <luquanhao@yeah.net>
2019-05-13 20:12:10 +0800

    code folder rename 2019/5/13 fir

---
+++ C++/PAT_Basic_Level/1030 完美数列.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N, p;
+    cin >> N >> p;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; ++i)
+        cin >> arr[i];
+
+    sort(arr.begin(), arr.end());
+
+    int len = N;
+
+    for (int i = 0; i < N - len + 1; ++i)
+        for (int j = i + len + 1; j < N; ++j)
+            if (arr[j] <= arr[i] * p)
+                len = j - i + 1;
+            else
+                break;
+
+    cout << len << endl;
+
+    system("pause");
+    return 0;
+}
