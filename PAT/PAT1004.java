455d42c4d6c720bd262bf9f7088a01ddbe00c472
NameIsTrouble <44404634+nameistrouble@users.noreply.github.com>
2019-05-28 11:05:43 +0800

    code synchronize 2019/5/28 sec

--- PAT/PAT1004.java
+++ PAT/PAT1004.java
@@ -1,39 +1,42 @@
-package Java.PAT;
+package  Java.PAT;
 
 import java.lang.reflect.Array;
 import java.util.*;
 import java.lang.String;
 
+class stu {
+    String name, ID;
+    int score;
+
+    stu(String nameT, String IDT, int scoreT) {
+        name = nameT;
+        ID = IDT;
+        score = scoreT;
+    }
+}
+
+class cmp implements Comparator<stu> {
+    public int compare(stu a, stu b) {
+        if (a.score > b.score) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
+
 public class PAT1004 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean vis[] = new boolean[10005];
-        Arrays.fill(vis, false);
         int n = in.nextInt();
-        int array[] = new int[n];
-
+        stu temp[] = new stu[10005];
         for (int i = 0; i < n; ++i) {
-            int temp = in.nextInt();
-            array[i] = temp;
-
-            while (temp != 1) {
-                if (temp % 2 != 0)
-                    temp = temp * 3 + 1;
-                temp /= 2;
-                if (vis[temp])
-                    break;
-                vis[temp] = true;
-            }
+            String nameT = in.next(), IDT = in.next();
+            int scoreT = in.nextInt();
+            temp[i] = new stu(nameT, IDT, scoreT);
         }
-        Arrays.sort(array);
-        boolean flag = false;
-        for (int i = n - 1; i >= 0; --i) {
-            if (array[i] == 0)
-                break;
-            if (!vis[array[i]]) {
-                System.out.printf("%s%d", flag ? " " : "", array[i]);
-                flag = true;
-            }
-        }
+        Arrays.sort(temp, 0, n, new cmp());
+        System.out.println(temp[0].name + " " + temp[0].ID);
+        System.out.println(temp[n - 1].name + " " + temp[n - 1].ID);
     }
 }
